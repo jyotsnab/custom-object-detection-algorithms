dfb490f3609bdf1e9e8c5f5a2f0ca06aefe31ec9
jyotsnab <jyotsna17101@gmail.com>
2018-06-07 15:19:00 +0200

    Haarcascade

---
+++ HAARcascade/training.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 09:07:15 2018
+
+@author: jyotsnab
+
+Haar Cascade for object detection
+"""
+
+#import libraries
+import os
+import subprocess
+import cv2
+import numpy as np
+
+
+
+
+# =============================================================================
+# #Positive Images
+# =============================================================================
+# resize to 100x100
+# change to grayscale
+# identify manually from labelImg and save as txt
+# save in the format positive_images/<image_name>
+
+
+
+
+# =============================================================================
+# #Negative Images
+# =============================================================================
+# collection file (bg.txt) : negatives.txt
+
+
+
+# =============================================================================
+# #Positive vector file
+# =============================================================================
+# os.system("sh training_steps.sh")
+
+
+# =============================================================================
+# #Train Cascade
+# =============================================================================
+
+
